cce6100f0fcd3d5b8ca88d9edd98f90190213f35
Chris Erin <chris.erin@gmail.com>
2025-02-13 23:05:20 +0000

    fix: assets.deploy

--- mix.exs
+++ mix.exs
@@ -69,10 +69,17 @@ defmodule Blog.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
+      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.build": ["tailwind demo", "esbuild demo"],
+      "assets.deploy": [
+        "tailwind demo --minify",
+        "esbuild demo --minify",
+        "phx.digest"
+      ]
     ]
   end
 end
